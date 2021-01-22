2125c6c6753299a32dc83a8b2a68b2ff98aa6782
ahmet9207 <ahmetakoguz@gmail.com>
2021-01-22 13:59:51 +0300

    Add project files.

---
+++ DictionaryOdev/MyDictionery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryOdev
+{
+    class MyDictionery<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionery()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+            
+            
+        public void Add(K itemK, V itemV)
+            
+        {
+            K[] tempArayk = keys;
+            V[] tempArayv = values;
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int k = 0; k < tempArayk.Length; k++)
+            {
+                keys[k] = tempArayk[k];
+                values[k] = tempArayv[k];
+            }
+           keys[keys.Length - 1] =  itemK;
+           values[values.Length - 1] = itemV;
+        }
+        
+       
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+
+        public K[] Keys
+        {
+            get { return keys; }
+        }
+
+        public V[] Values
+        {
+            get { return values; }
+        }
+
+
+    }
+}
---
+++ DictionaryOdev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionery<int,string> numara = new MyDictionery<int,string>();
+            numara.Add (1,"AHMET");
+            numara.Add(2,"MEHMET");
+            numara.Add(3, "AYŞE");
+            numara.Add(4, "FATMA");
+
+
+
+            for (int i = 0; i < numara.Keys.Length; i++)
+                Console.WriteLine("Sıra No:"+numara.Keys[i]+"        İsim:"+ numara.Values[i]);
+
+        }
+    }
+}
